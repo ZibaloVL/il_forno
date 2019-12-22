fa28f595c73f3581bd70a70a0943836694560dfd
ZibaloV <fotoroom.md@gmail.com>
2019-12-22 18:46:46 +0200

    befor mdi

---
+++ src/components/HeaderSait.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar app hide-on-scroll color="#ffffff">
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="../assets/logo/logoFB.jpg"
+        transition="scale-transition"
+        width="40"
+      />
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "HeaderSait",
+
+  data: () => ({})
+};
+</script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-parallax dark height="1080" src="../assets/bg/20191206-IMG_1144.jpg">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="h1 mb-4 py-10">НЕАПОЛИТАНСКИЕ РЕЦЕПТЫ</h1>
+        </v-col>
+      </v-row>
+    </v-parallax>
+    <div>ghbdtn</div>
+    <v-parallax dark height="1080" src="../assets/bg/20191206-IMG_1195.jpg">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="h1 mb-4 py-10">ЛУЧШИЕ ПРОДУКТЫ</h1>
+        </v-col>
+      </v-row>
+    </v-parallax>
+    <v-parallax dark height="1080" src="../assets/bg/20191219-IMG_2540.jpg">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="h1 mb-4 py-10">ИЗЫСКАНОЕ МЕНЮ</h1>
+        </v-col>
+      </v-row>
+    </v-parallax>
+    <v-parallax dark height="1080" src="../assets/bg/20191219-IMG_2571-Edit.jpg">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="h1 mb-4 py-10">НЕЗАБЫВАЕМЫЕ ДЕСЕРТЫ</h1>
+        </v-col>
+      </v-row>
+    </v-parallax>
+    <v-parallax dark height="1080" src="../assets/bg/20191218-IMG_2381.jpg">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="h1 mb-4 py-10">ВОЛШЕБНЫЕ НАПИТКИ</h1>
+        </v-col>
+      </v-row>
+    </v-parallax>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+
+  data: () => ({})
+};
+</script>
+
+<style  scoped>
+.h1 {
+  font-size: 4rem !important;
+}
+</style>
