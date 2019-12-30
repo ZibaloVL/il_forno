da73dc31d297ce0f95227e257850b185aa1ca645
ZibaloVL <41912546+ZibaloVL@users.noreply.github.com>
2019-12-30 04:25:54 +0200

    response footer path

--- src/components/FoterSait.vue
+++ src/components/FoterSait.vue
@@ -4,13 +4,13 @@
       <v-card-title class="white black--text">
         <v-container>
           <v-row>
-            <v-col id="contact" class="text-center black--text">
+            <v-col  id="contact" cols="12" md="6" xs="12" class="text-center black--text">
               <h3>Contacte:</h3>
               <h4>Strada Alexei Şciusev 98, Chișinău, Moldova</h4>
               <h4>tel. +373 22 692 19 219</h4>
               <h4>email: ilForno@ilForno.ilforno</h4>
             </v-col>
-            <v-col id="map">
+            <v-col  id="map" cols="12" md="6" xs="12">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d679.9494403637511!2d28.82060852924254!3d47.024574612907415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c97d611068d17b%3A0xdab911a743ee10c7!2sIl%20Forno!5e0!3m2!1sru!2s!4v1577345974547!5m2!1sru!2s"
                 width="100%"
